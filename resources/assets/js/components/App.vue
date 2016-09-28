2bb7fb7d9850944fc1b6c8ad55231c732b029d3b
Jamie <me@something.com>
2016-09-28 16:28:13 +0100

    initial

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="clasico">
+        <grid></grid>
+    </div>
+</template>
+
+<script>
+
+    import Grid from './Grid.vue';
+
+    export default {
+        components: { Grid }
+    }
+</script>
---
+++ resources/assets/js/components/Grid.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="table">
+        <table>
+            <thead>
+                <tr>
+                    <th
+                        v-for="key in columns"
+                    >
+                        {{ key | capitalize }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                    v-for="(rowIndex, entry) in data"
+                >
+                    <td v-for="(columnIndex, key) in columns"
+                        class="cell"
+                        :class="{activeRow: rowIndex == activeRowIndex, activeColumn: columnIndex == activeColumnIndex}"
+                    >
+                        <div class="view">
+                            <label @click="makeCellActive(rowIndex, columnIndex)">
+                                {{ entry[key] }}
+                            </label>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+
+    Vue.use(require('vue-resource'));
+
+    import io from 'socket.io-client';
+    const socket = io('http://192.168.10.10:8081');
+
+    import { updateActiveCellPosition } from '../vuex/actions';
+
+    export default {
+        vuex: {
+            actions: {
+                updateActiveCellPosition
+            },
+            getters: {
+                colums
+            }
+        },
+        ready() {
+            socket.io('clicked-cell-channel:App\\Events\\UserChangedActiveCell', (data) => {
+                this.updateActiveCellPosition(data.rowIndex, data.columnIndex);
+            });
+        },
+        methods: {
+            makeCellActive(rowIndex, columnIndex) {
+                console.log(rowIndex, columnIndex);
+
+                Vue.http.post('/api/updateActiveCellPosition', {rowIndex, columnIndex});
+            }
+        }
+    }
+</script>
+
+<style>
+table {
+  border: 2px solid #42b983;
+  border-radius: 3px;
+  background-color: #fff;
+}
+
+th {
+  background-color: #42b983;
+  color: rgba(255,255,255,0.66);
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -user-select: none;
+}
+
+td {
+  background-color: #f9f9f9;
+}
+
+th, td {
+  min-width: 120px;
+  padding: 10px 20px;
+}
+
+th.active {
+  color: #fff;
+}
+
+th.active .arrow {
+  opacity: 1;
+}
+
+.arrow {
+  display: inline-block;
+  vertical-align: middle;
+  width: 0;
+  height: 0;
+  margin-left: 5px;
+  opacity: 0.66;
+}
+
+.arrow.asc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-bottom: 4px solid #fff;
+}
+
+.arrow.dsc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 4px solid #fff;
+}
+
+#search {
+  margin-bottom: 10px;
+}
+</style>
