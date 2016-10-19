8c7eb09b0e0fd8d362e51ed2a5a5e5d8aeaf2a28
NO <you@example.com>
2016-10-19 17:29:48 +0100

    omg it's working pretty well now

--- app/Services/ReviewStorer.php
+++ app/Services/ReviewStorer.php
@@ -2,11 +2,16 @@
 
 namespace App\Services;
 
-use App\Review;
+use Auth;
+use Carbon\Carbon;
+use App\Review; use App\Reviewable; use App\Type;
 
 class ReviewStorer
 {
     public $input;
+
+    private $type;
+    private $reviewable;
 
     public static function make($input)
     {
@@ -19,12 +24,48 @@ class ReviewStorer
 
     public function handle()
     {
-        dd($this->input);
+        $this->checkForNewType();
 
+        $this->checkForNewReviewable();
+
+        $this->writeReview();
+    }
+
+    private function checkForNewType()
+    {
+        if ($this->input['new_type']) {
+            $this->type = Type::create([
+                'name' => $this->input['new_type']
+            ]);
+
+            return;
+        }
+
+        $this->type = Type::find($this->input['type']);
+    }
+
+    private function checkForNewReviewable()
+    {
+        if ($this->input['new_reviewable']) {
+            $this->reviewable = Reviewable::create([
+                'title' => $this->input['new_reviewable'],
+                'creator' => 'test',
+                'year_made' => Carbon::now(),
+                'type_id' => $this->type->id
+            ]);
+
+            return;
+        }
+
+        $this->reviewable = Reviewable::find($this->input['reviewable']);
+    }
+
+    private function writeReview()
+    {
         Review::create([
             'user_id' => Auth::user()->id,
-            'reviewable_id' => $request->reviewable,
-            'body' => $request->body
+            'reviewable_id' => $this->reviewable->id,
+            'body' => $this->input['body']
         ]);
     }
 }
